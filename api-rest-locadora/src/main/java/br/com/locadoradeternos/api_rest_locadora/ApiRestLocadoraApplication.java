aabb44efe4eadbcf742d854ece04324f802afc81
Morita <joao@teste.com>
2024-09-12 09:27:12 -0300

    Buildando o spring com as dependencias para o projeto

---
+++ api-rest-locadora/src/main/java/br/com/locadoradeternos/api_rest_locadora/ApiRestLocadoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.locadoradeternos.api_rest_locadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestLocadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestLocadoraApplication.class, args);
+	}
+
+}
---
+++ api-rest-locadora/src/test/java/br/com/locadoradeternos/api_rest_locadora/ApiRestLocadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.locadoradeternos.api_rest_locadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestLocadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
